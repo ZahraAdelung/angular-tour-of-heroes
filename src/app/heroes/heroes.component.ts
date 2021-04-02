edaac38dbb382babdaa3c0501e7fd6df3362d6be
Zahra Adelung <zahra.adelung@gmail.com>
2021-04-02 10:26:08 +0200

    The hero editor

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero'
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent {
+
+  public hero: Hero = {
+    id: 1,
+    name: 'Windstorm',
+  }
+}
+
+
+
+

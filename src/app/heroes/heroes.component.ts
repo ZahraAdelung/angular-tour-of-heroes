8dee8cbcdbb3ac4a3cb2115c302c06c463f15fe6
Zahra Adelung <zahra.adelung@gmail.com>
2021-04-02 14:02:23 +0200

    Display a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero'
+import { HEROES } from "../mock-heroes";
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +9,14 @@ import { Hero } from '../hero'
 })
 export class HeroesComponent {
 
-  public hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
+  heroes = HEROES;
+  public selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
+
+
 }
 
 
